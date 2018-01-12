0e3430f61841553dc79c5cb927468bdf5b7995b9
WW930\A655988 <sara.pstrowska.external@atos.net>
2018-01-12 22:50:18 +0100

    continuing working on Vuex

--- Vuex/src/components/Counter.vue
+++ Vuex/src/components/Counter.vue
@@ -1,17 +1,17 @@
 <template>
     <div>
-        <button class="btn btn-primary" @click="increment">Increment</button>
-        <button class="btn btn-primary" @click="decrement">Decrement</button>
+        <button class="btn btn-danger" @click="asyncIncrement({by: 50, duration: 3000})">Increment</button>
+        <button class="btn btn-danger" @click="asyncDecrement({by: 50, duration: 3000})">Decrement</button>
     </div>
 </template>
 
 <script>
-    import { mapMutations } from 'vuex';
+    import { mapActions } from 'vuex';
     export default {
         methods: {
-            ...mapMutations([
-                'increment',
-                'decrement'
+            ...mapActions([
+                'asyncIncrement',
+                'asyncDecrement'
             ])
         }
     }
---
+++ Vuex/src/store/actions.js
@@ -0,0 +1,6 @@
+export const updateValue = ({commit}, payload) => {
+    commit('updateValue', payload)
+}
+export const updateModelValue = ({commit}, payload) => {
+    commit('updateModelValue', payload);
+}
